49a8a2679454b07087b09f3050847490dc6198c9
Diana-Bernardos <dianilla75@gmail.com>
2024-10-02 20:08:15 +0200

    new landing

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-// Smooth scroll functionality for navigation links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+const themeButton = document.getElementById('theme-button');
+const themeIcon = document.getElementById('theme-icon');
+const menuButton = document.querySelector('.menu-button');
+const dropdown = document.querySelector('.dropdown');
 
-        const sectionId = this.getAttribute('href').substring(1);
-        document.getElementById(sectionId).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+menuButton.addEventListener('click', () => {
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 });
+
+// Al hacer clic en el botón de tema
+themeButton.addEventListener('click', () => {
+    // Alternar entre el tema claro y oscuro
+    document.body.classList.toggle('light-theme');
+    
+    // Cambiar el icono dependiendo del tema actual
+    if (document.body.classList.contains('light-theme')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun'); // Cambiar a icono de sol
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon'); // Volver a icono de luna
+    }
+});
+
+
+
 
