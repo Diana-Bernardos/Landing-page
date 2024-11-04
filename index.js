84eda823eed687f8a9c91188946f7f6ad65f6260
Diana-Bernardos <dianilla75@gmail.com>
2024-11-04 14:06:39 +0100

    new1

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const themeButton = document.getElementById('theme-button');
 const themeIcon = document.getElementById('theme-icon');
 const menuButton = document.querySelector('.menu-button');
-const dropdown = document.querySelector('.dropdown');
+
 
 function toggleMenu() {
     const dropdown = document.querySelector('.dropdown');
     dropdown.classList.toggle('show');
 }
 
-// Al hacer clic en el botón de tema
-themeButton.addEventListener('click', () => {
-    // Alternar entre el tema claro y oscuro
-    document.body.classList.toggle('light-theme');
-    
-    // Cambiar el icono dependiendo del tema actual
-    if (document.body.classList.contains('light-theme')) {
-        themeIcon.classList.remove('fa-moon');
-        themeIcon.classList.add('fa-sun'); // Cambiar a icono de sol
-    } else {
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon'); // Volver a icono de luna
-    }
-});
+
 
 
 
